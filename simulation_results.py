e27b8e32a082cf6e0a28da34c8a5a167e77111c3
Piyanut-Rat <piyanut.ratpibun@gmail.com>
2021-03-30 18:09:03 +0700

    Update simulation_results.py

--- simulation_results.py
+++ simulation_results.py
@@ -4,11 +4,8 @@ sys.path.append("Hass_2017")
 from biomass import run_simulation
 
 import Hass_2017
-#import fitness
+
 model = Hass_2017.create()
-
-
-
 run_simulation(model, viz_type='original')
 
 
