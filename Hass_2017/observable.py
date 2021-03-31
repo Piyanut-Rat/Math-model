dd549389151982c20fc6e009ff116e86664f64e9
Piyanut-Rat <piyanut.ratpibun@gmail.com>
2021-03-31 23:40:31 +0700

    Update observable.py

--- Hass_2017/observable.py
+++ Hass_2017/observable.py
@@ -108,7 +108,7 @@ class NumericalSimulation(DifferentialEquation):
                 )
                 self.simulations[observables.index('FACS_Met'), :, i] = np.log10(sol.y[V.Met, :] + 2*sol.y[V.pMetd, :] + sol.y[V.pMetEGFR, :] + sol.y[V.pMetErbB3, :]
                 )
-                
+                '''
                 self.simulations[observables.index('tEGFR_au'), :, i] = np.log10(
                     x[C.offset_tEGFR_CelllineH322M] + x[C.scale_tEGFR_CelllineH322M]* (sol.y[V.EGFR, :] + sol.y[V.EGFR_EGF, :] + sol.y[V.EGFR_BTC, :] + sol.y[V.EGFRi, :] + 
                     2*sol.y[V.pEGFRd, :] + 2*sol.y[V.pEGFRi, :] + 2*sol.y[V.pEGFRi_ph, :] + sol.y[V.pErbB12i, :] + sol.y[V.pErbB13i, :] + 
@@ -129,7 +129,7 @@ class NumericalSimulation(DifferentialEquation):
                     x[C.offset_tIGF1R_CelllineH322M] + x[C.scale_tIGF1R_CelllineH322M] *(sol.y[V.IGF1R, :] + sol.y[V.IGF1R_IGF1, :] + sol.y[V.IGF1Ri, :] + 2*sol.y[V.pIGF1Rd, :] + 
                     2*sol.y[V.pIGF1Ri, :] + 2*sol.y[V.pIGF1Ri_ph, :])
                 )
-                '''
+                
                 self.simulations[observables.index('pEGFR_au'), :, i] = np.log10(x[C.offset_pEGFR_CelllineH322M] + x[C.scale_pEGFR_CelllineH322M] * (2*sol.y[V.pEGFRd, :] + 2*sol.y[V.pEGFRi, :] + 2*sol.y[V.pEGFRi_ph, :] + sol.y[V.pErbB12i, :] + 
                     sol.y[V.pErbB13i, :] + sol.y[V.pErbB12, :] + sol.y[V.pErbB13, :] + sol.y[V.pErbB12i_ph, :] + sol.y[V.pErbB13i_ph, :] + sol.y[V.pMetEGFR, :] + sol.y[V.pMetEGFRi, :] + sol.y[V.pMetEGFRi_ph, :])
                 )
@@ -137,7 +137,7 @@ class NumericalSimulation(DifferentialEquation):
                 )
                 self.simulations[observables.index('pErbB3_au'), :, i] = np.log10(x[C.offset_pErbB3_CelllineH322M] + x[C.scale_pErbB3_CelllineH322M] * (2*sol.y[V.pErbB3d, :] + 2*sol.y[V.pErbB3i, :] + 2*sol.y[V.pErbB3i_ph, :] + sol.y[V.pErbB32, :] + sol.y[V.pErbB13, :] + sol.y[V.pErbB32i, :] + sol.y[V.pErbB13i, :] + sol.y[V.pErbB32i_ph, :] + sol.y[V.pErbB13i_ph, :] + sol.y[V.pMetErbB3, :] + sol.y[V.pMetErbB3i, :] + sol.y[V.pMetErbB3i_ph, :])
                 )
-                '''
+                
                 self.simulations[observables.index('pIGF1R_au'), :, i] = np.log10(
                     x[C.offset_pIGF1R_CelllineH322M] + x[C.scale_pIGF1R_CelllineH322M] * (2*sol.y[V.pIGF1Rd, :] + 2*sol.y[V.pIGF1Ri, :] + 2*sol.y[V.pIGF1Ri_ph, :])
                 )
@@ -149,16 +149,16 @@ class NumericalSimulation(DifferentialEquation):
 
                 self.simulations[observables.index('pMEK_au'), :, i] = np.log10(x[C.offset_pMEK_CelllineH322M] + x[C.scale_pMEK_CelllineH322M] * sol.y[V.pMEK, :]
                 )
-                '''
+                
                 self.simulations[observables.index('pERK_au'), :, i] = np.log10(x[C.offset_pERK_CelllineH322M] + x[C.scale_pERK_CelllineH322M] * sol.y[V.pERK, :]
                 )
 
                 self.simulations[observables.index('pAKT_au'), :, i] = np.log10(x[C.offset_pAKT_CelllineH322M] + x[C.scale_pAKT_CelllineH322M] * sol.y[V.pAKT, :]
                 )
-                '''
+                
                 self.simulations[observables.index('pS6K1_au'), :, i] = np.log10(x[C.offset_pS6K1_CelllineH322M] + x[C.scale_pS6K1_CelllineH322M] * sol.y[V.pS6K1, :] 
                 )
-                '''
+                
                 self.simulations[observables.index('pS6_au'), :, i] = np.log10(x[C.offset_pS6_CelllineH322M] + (x[C.scale_pS6_CelllineH322M]* sol.y[V.pS6, :])
                 )
                 
