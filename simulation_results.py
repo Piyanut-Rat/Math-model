23c535218890688fefa0d48b534b43adc063a5c5
Piyanut-Rat <piyanut.ratpibun@gmail.com>
2021-04-08 22:54:49 +0700

    Update simulation_results.py

--- simulation_results.py
+++ simulation_results.py
@@ -1,6 +1,5 @@
 import sys
 import os
-#from IPython.display import Image, display_png
 sys.path.append("Hass_2017")
 
 from biomass import run_simulation
@@ -8,23 +7,9 @@ from biomass import run_simulation
 import Hass_2017
 
 model = Hass_2017.create()
-#run_simulation(model, viz_type='original')
-run_simulation(model, viz_type="original", save_format="png",show_all=True)
+#run_simulation(model, viz_type='original') #defult pdf format
+run_simulation(model, viz_type="original", save_format="png")
+#run_simulation(model, viz_type="original", save_format="png",show_all=True)
 #run_simulation(model, viz_type="average", save_format="png")
 #run_simulation(model, viz_type="best", save_format="png")
 #run_simulation(model, viz_type="experiment", save_format="png")
-'''
-for observable in model.obs:
-    with open(
-        os.path.join(
-            model.path,
-            "figure",
-            "simulation",
-            "original",
-            f"{observable}.png",
-        ),
-        mode="rb",
-    ) as f:
-        display_png(Image(f.read()))
-
-'''
